9d7e5a9cad4fea542ae6a9b94dccbb4180c7e44d
Happy Hinson <Happyhnspro@gmail.com>
2026-03-22 18:26:00 +0100

    Récupération  DOM elements for student input

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 // script principal
 
+const studentName = document.getElementById("studentName");// Récupération de l'input pour le nom de l'étudiant
+const addStudentBtn = document.getElementById("addStudentBtn"); // Récupération du bouton pour ajouter un étudiant
