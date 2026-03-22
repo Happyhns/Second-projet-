291a6d821493bc50be87808da4a54adb1aa7b881
Happy Hinson <Happyhnspro@gmail.com>
2026-03-22 19:37:29 +0100

    Création fonction addStudent

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 
 const studentName = document.getElementById("studentName");// Récupération de l'input pour le nom de l'étudiant
 const addStudentBtn = document.getElementById("addStudentBtn"); // Récupération du bouton pour ajouter un étudiant
+
+
+function addStudent() {
+    console.log("fonction appelée"); // pour vérifier que la fonction est bien appelée
+}
